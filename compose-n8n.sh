84f33f02f4cd7ee03f9774abaf1f9d5b4a28f66e
hichock <34815919+hichock@users.noreply.github.com>
2024-12-28 10:27:14 -0300

    Update compose-n8n.sh

--- compose-n8n.sh
+++ compose-n8n.sh
@@ -4,11 +4,7 @@ sudo docker compose up -d --force-recreate
 
 # Step 7: Install custom npm libraries inside the n8n container
 echo "Installing external npm libraries inside the n8n container..."
-sudo docker compose exec -u root n8n sh -c "
-  mkdir -p /usr/local/lib/node_modules &&
-  npm config set prefix /usr/local/lib/node_modules &&
-  npm install -g axios moment uuid pg json-schema-generator openai @supabase/supabase-js
-"
+sudo docker compose exec -u root n8n npm install -g axios moment uuid pg json-schema-generator openai @supabase/supabase-js telegram
 
 echo "Installation of npm packages done."
 echo "N8N setup is complete and ready for use."
