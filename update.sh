f4a1e8b5240ef7a47db7deebfca6ddad7d68d148
hichock <34815919+hichock@users.noreply.github.com>
2025-08-14 02:06:21 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 sudo docker compose pull
 sudo docker compose down --rmi all --volumes --remove-orphans
+sudo docker volume rm caddy_data n8n_data npm_data
 sudo docker volume create caddy_data && sudo docker volume create n8n_data && sudo docker volume create npm_data
 sudo docker compose up -d --force-recreate
 sudo docker compose exec -u root n8n npm install -g axios moment uuid pg json-schema-generator openai @supabase/supabase-js
